15c3bca7f6353be20e521589c22986387d05cd99
zeegrafics <53975994+zeegrafics@users.noreply.github.com>
2019-10-14 22:38:01 -0400

    my New Website
    my New Website

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Java Script");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
